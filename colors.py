c623798064b32fce467864d47049ddad2fc8d456
andrKonan <52403327+andrKonan@users.noreply.github.com>
2020-06-21 18:53:32 +0000

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,10 @@
+clRed = (255, 0, 0)
+clGreen = (0, 255, 0)
+clBlue = (0, 0, 255)
+
+clCyan = (0, 255, 255)
+clMagenta = (255, 0, 255)
+clYellow = (255, 255, 0)
+
+clWhite = (255, 255, 255)
+clBlack = (0, 0, 0)
